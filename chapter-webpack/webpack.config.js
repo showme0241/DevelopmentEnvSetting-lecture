999eead3eb3fee3852b81318bce8a6327f409182
beenbin92 <showme0241@gmail.com>
2023-10-15 21:04:41 +0900

    :package: Webpack : webpack's loader
    p.s 웹팩을 통해 파일이 번들링되는 것은 loader가 중간에서 변환하는 작업을 거쳐주기 때문이다.

--- chapter-webpack/webpack.config.js
+++ chapter-webpack/webpack.config.js
@@ -11,4 +11,13 @@ module.exports = {
         // entry key가 name으로 치환됨
         filename: "[name].js",
     },
+    module: {
+        rules: [
+            {
+                // 모든 js 확장자를 가지고 있는 파일을 읽는다.
+                test: /\.js$/,
+                use: [path.resolve("./my-webpack-loader.js")],
+            },
+        ],
+    },
 };
